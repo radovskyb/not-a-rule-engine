acf93d69c55e7cfd549ef6966b7ad9db2c9a49e0
Benjamin Radovsky <benjamin@Benjamins-MacBook-Pro.local>
2025-12-27 04:28:05 +1100

    Adjust mux.Handle after seeing mux changes from go1.22 or so

--- routes/routes.go
+++ routes/routes.go
@@ -10,11 +10,7 @@ func Setup(h *handler.Handler) *http.ServeMux {
 	// Base handler.
 	mux := http.NewServeMux()
 
-	mux.Handle("/api/ingest", h.Serve(h.Ingest))
+	mux.Handle("POST /api/ingest", h.Serve(h.Ingest))
 
 	return mux
 }
-
-// Oh geez, I can't remember, but I'm starting to think the default router doesn't have fncs for request type like gorilla/mux.
-//
-// TODO: Create a wrapper to differentiate between POST, GET, etc.
